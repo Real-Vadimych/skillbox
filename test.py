af4f22e34746e371eee66ea8fe987f32134d3baa
VVK <vadim.kotelenets@gmail.com>
2022-02-17 22:41:05 +0300

    12

--- test.py
+++ test.py
@@ -1,18 +1,3 @@
-words =[]
-with open('anna_words.txt', encoding='UTF-8') as file:
-    words = [word.strip() for word in file]
+import pandas as pd
 
-
-
-len_words = {}
-for word in words:
-    lenght = len(word)
-    if lenght in len_words:
-        len_words[lenght] += 1
-    else:
-        len_words[lenght] = 1
-
-sorted_words = {k: len_words[k] for k in sorted(len_words)}
-
-for k, v in sorted_words.items():
-    print(f'{k}; {v}')
+data = [[]]
